1442ed1156861558eefb527dac2ee28188b37fdc
aamir12 <aamirkhan8878@gmail.com>
2023-06-21 19:39:20 +0530

    update

--- src/Child.js
+++ src/Child.js
@@ -1,17 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./style.css";
 
 export default function Child() {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState(null);
 
   const increment = () => {
     setCount(count + 1);
   };
 
+  //Error boundaries catch errors during rendering, in lifecycle methods, and in constructors of the whole tree below them.
+
   //it will not work -- Event handler
   const errorGenerator = () => {
     throw new Error("Error Generator");
   };
+
+  //Asynchronous code (e.g. setTimeout or requestAnimationFrame callbacks)
+  const fetchData = useCallback(async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/users-err/1"
+    );
+
+    const responseData = await response.json();
+    if (responseData.user.id) {
+      console.log("do something");
+      setData(responseData);
+    }
+  }, []);
 
   useEffect(() => {
     if (count === 3) {
@@ -19,11 +35,17 @@ export default function Child() {
     }
   }, [count]);
 
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div>
       <h1>Child Component</h1>
       <button onClick={increment}>increment {count}</button>
-      <button onClick={errorGenerator}>Error</button>
+      <br />
+      <button onClick={errorGenerator}>Error not work event Handler</button>
+      {JSON.stringify(data)}
     </div>
   );
 }
